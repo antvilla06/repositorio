1ba57adbbdd4105499b3384b3562c8fea792b5f9
antvilla06 <antvilla06@gmail.com>
2018-05-04 14:10:39 -0500

    Se modificó linea 25

--- pythonprueba1.py
+++ pythonprueba1.py
@@ -23,7 +23,7 @@ class Empleados:
 empleado1 = Empleados('Pedro', 'Porras', 14000)
 empleado2 = Empleados('Miguel', 'Ayala', 2000)
 empleado3 = Empleados('Toño', 'Villa', 15000)
-
+empleado4 = Empleados('Too', 'Villa', 15000)
 print(empleado1.fullname())
 print(empleado2.__dict__)
 print(Empleados.__dict__)
